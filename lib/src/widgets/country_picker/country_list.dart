ad01229f32ad3ab656811b98d283b727718b5154
emri99 <mery.cyril@gmail.com>
2021-09-05 10:56:53 +0200

    feat(contry-selector): add favorites countries & few others props
    * add properties :
      * favoritesCountries : list of isoCode to be displayed on top of the list
      * sortCountries : whether country should be order by localised name
      * addSeparator : whether a divider should be added between favorites and defaults countries
      * showDialCode : whether the dial code should be displayed as ListTile subtitle
      * noResultMessage : the message displayed instead of the list when the search has no results
    * add proxy properties for above properties in selector navigator classes
    * add API docs on updated files
    * fix countryFinder mismatch order on cleaning search after performing a search

--- lib/src/widgets/country_picker/country_list.dart
+++ lib/src/widgets/country_picker/country_list.dart
@@ -2,48 +2,77 @@ import 'package:circle_flags/circle_flags.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import '../../localization/phone_field_localization.dart';
 import '../../models/country.dart';
 
 class CountryList extends StatelessWidget {
+  /// Callback function triggered when user select a country
   final Function(Country) onTap;
+
+  /// List of countries to display
   final List<Country> countries;
+
+  /// proxy to the ListView.builder controller (ie: [ScrollView.controller])
   final ScrollController? scrollController;
+
+  /// whether the country dialcode should be displayed as the [ListTile.subtitle]
+  final bool showDialCode;
+
+  /// the index of the listview where divider should be added
+  final int? separatorIndex;
 
   const CountryList({
     Key? key,
     required this.countries,
     required this.onTap,
     this.scrollController,
+    this.separatorIndex = null,
+    this.showDialCode = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final int listLength = countries.isNotEmpty && separatorIndex != null
+        ? countries.length + 1
+        : countries.length;
+
     return ListView.builder(
       controller: scrollController,
       shrinkWrap: true,
-      itemCount: countries.length,
+      itemCount: listLength,
       itemBuilder: (BuildContext context, int index) {
-        final country = countries[index];
+        if (index == separatorIndex) {
+          return Divider(key: Key('countryListSeparator.$hashCode'));
+        }
+
+        // when separator is reached, the country list index is shift
+        // by 1 from the list builder index
+        final countryIndexDelta =
+            separatorIndex != null && index >= separatorIndex! ? 1 : 0;
+        Country country = countries[index - countryIndexDelta];
+
         return ListTile(
           key: Key(country.isoCode),
-          leading: CircleFlag(country.isoCode),
+          leading: CircleFlag(
+            country.isoCode,
+            size: showDialCode ? null : 40,
+          ),
           title: Align(
             alignment: AlignmentDirectional.centerStart,
             child: Text(
-              PhoneFieldLocalization.of(context)?.translate(country.isoCode) ??
-                  country.name,
+              country.localisedName(context),
               textAlign: TextAlign.start,
             ),
           ),
-          subtitle: Align(
-            alignment: AlignmentDirectional.centerStart,
-            child: Text(
-              country.displayDialCode,
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.start,
-            ),
-          ),
+          subtitle: showDialCode
+              ? Align(
+                  alignment: AlignmentDirectional.centerStart,
+                  child: Text(
+                    country.displayDialCode,
+                    textDirection: TextDirection.ltr,
+                    textAlign: TextAlign.start,
+                  ),
+                )
+              : null,
           onTap: () => onTap(country),
         );
       },
--- lib/src/widgets/country_picker/country_selector.dart
+++ lib/src/widgets/country_picker/country_selector.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:phone_form_field/src/helpers/country_finder.dart';
-import 'package:phone_form_field/src/models/all_countries.dart';
-import 'package:phone_form_field/src/widgets/country_picker/search_box.dart';
 
+import '../../helpers/country_finder.dart';
+import '../../localization/phone_field_localization.dart';
+import '../../models/all_countries.dart';
 import '../../models/country.dart';
 import 'country_list.dart';
+import 'search_box.dart';
+
+const _emptyFavCountriesArray = <String>[];
 
 class CountrySelector extends StatefulWidget {
+  /// List of countries to display in the selector
+  /// Value optional in constructor.
+  /// when omitted, the full country list is displayed
   final List<Country> countries;
+
+  /// Callback triggered when user select a country
   final Function(Country) onCountrySelected;
+
+  /// ListView.builder scroll controller (ie: [ScrollView.controller])
   final ScrollController? scrollController;
+
+  /// Sort the countries automatically by localized name.
+  /// Note that the favorites countries are not sorted but
+  /// displayed in defined order.
+  final bool sortCountries;
+
+  /// Determine the countries to be displayed on top of the list
+  /// Check [addSeparator] property to enable/disable adding a
+  /// list divider between favorites and others defaults countries
+  final List<String> favoritesCountries;
+
+  /// Whether to add a list divider between favorites & defaults
+  /// countries.
+  final bool addSeparator;
+
+  /// Whether to show the country dial code (ie: +1 / +33 /...)
+  /// as a listTile subtitle
+  final bool showDialCode;
+
+  /// The message displayed instead of the list when the search has no results
+  final String noResultMessage;
 
   CountrySelector({
     Key? key,
     required this.onCountrySelected,
     this.scrollController,
+    this.sortCountries = false,
+    this.addSeparator = true,
+    this.showDialCode = false,
+    List<String>? favoritesCountries,
+    String? noResultMessage,
     List<Country>? countries,
   })  : countries = countries ?? allCountries,
+        noResultMessage = noResultMessage ?? 'No result found',
+        favoritesCountries = favoritesCountries ?? _emptyFavCountriesArray,
         super(key: key);
 
   @override
@@ -25,17 +63,62 @@ class CountrySelector extends StatefulWidget {
 
 class _CountrySelectorState extends State<CountrySelector> {
   late List<Country> _filteredCountries;
-  late final _countryFinder;
+  late CountryFinder _countryFinder;
+  int? _separatorIndex;
 
   @override
-  initState() {
-    super.initState();
-    _filteredCountries = widget.countries;
-    _countryFinder = CountryFinder(widget.countries);
+  didChangeDependencies() {
+    super.didChangeDependencies();
+    // ensure countries list is sorted by localized name
+    // this need to be done in didChangeDependencies (not in initState)
+    // as context is not available in initState and context is required
+    // to get the localized country name
+    _filteredCountries = widget.sortCountries
+        ? _sortCountries(widget.countries)
+        : widget.countries;
+
+    _countryFinder = CountryFinder(_filteredCountries);
+    _handleFavoritesCountries();
+  }
+
+  List<Country> _sortCountries(List<Country> countriesList) {
+    // perform a copy so we don't modify original value
+    return countriesList
+      ..sort((Country a, Country b) {
+        return a.localisedName(context).toLowerCase().compareTo(
+              b.localisedName(context).toLowerCase(),
+            );
+      });
+  }
+
+  _handleFavoritesCountries() {
+    final hasFavoritesCountry =
+        _filteredCountries.isNotEmpty && widget.favoritesCountries.isNotEmpty;
+
+    // hold index where the separator must be displayed
+    _separatorIndex = null;
+
+    if (!hasFavoritesCountry) {
+      return;
+    }
+
+    widget.favoritesCountries.reversed.forEach((String isoCode) {
+      final int favIndex = _filteredCountries.indexWhere(
+        (Country country) => country.isoCode == isoCode.toUpperCase(),
+      );
+      if (favIndex >= 0) {
+        _filteredCountries.removeAt(favIndex);
+        _filteredCountries.insert(0, Country(isoCode.toUpperCase()));
+        _separatorIndex = (_separatorIndex ?? 0) + 1;
+      }
+    });
   }
 
   _onSearch(String txt) {
-    setState(() => _filteredCountries = _countryFinder.filter(txt, context));
+    setState(() {
+      _filteredCountries = _countryFinder.filter(txt, context);
+      _handleFavoritesCountries();
+    });
   }
 
   @override
@@ -50,13 +133,22 @@ class _CountrySelectorState extends State<CountrySelector> {
           ),
         ),
         Flexible(
-          child: CountryList(
-            countries: _filteredCountries,
-            onTap: (country) {
-              widget.onCountrySelected(country);
-            },
-            scrollController: widget.scrollController,
-          ),
+          child: _filteredCountries.isNotEmpty
+              ? CountryList(
+                  countries: _filteredCountries,
+                  separatorIndex: _separatorIndex,
+                  showDialCode: widget.showDialCode,
+                  onTap: (country) {
+                    widget.onCountrySelected(country);
+                  },
+                  scrollController: widget.scrollController,
+                )
+              : Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text(PhoneFieldLocalization.of(context)
+                          ?.translate(widget.noResultMessage) ??
+                      widget.noResultMessage),
+                ),
         ),
       ],
     );
