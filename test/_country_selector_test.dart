166f96e4fcb8446b9f263b6cdba1ca6db9fcce41
cedvdb <cedvandenbosch@gmail.com>
2021-09-15 14:04:46 +0200

    testing

--- test/_country_selector_test.dart
+++ test/_country_selector_test.dart
@@ -2,39 +2,75 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:phone_form_field/phone_form_field.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:phone_form_field/src/widgets/country_picker/search_box.dart';
 
 void main() {
   group('CountrySelector', () {
-    final builder = ({
-      List<String>? favorites,
-      bool addFavoritesSeparator = false,
-    }) =>
-        MaterialApp(
-          locale: Locale('en'),
-          localizationsDelegates: [
-            ...GlobalMaterialLocalizations.delegates,
-            PhoneFieldLocalization.delegate,
-          ],
-          supportedLocales: [Locale('en')],
-          home: Scaffold(
-            body: Container(),
-          ),
-        );
+    group('Without internationalization', () {
+      final app = MaterialApp(
+        home: Scaffold(
+          body: CountrySelector(onCountrySelected: (c) {}),
+        ),
+      );
 
-    // the test will fail if the second test is uncommented, it's the exact same test
-    testWidgets('should be properly sorted without favorites', (tester) async {
-      await tester.pumpWidget(builder());
-      await tester.pumpAndSettle();
-      final scaffold = find.byType(Scaffold);
-      expect(scaffold, findsOneWidget);
+      testWidgets('Should filter with text', (tester) async {
+        await tester.pumpWidget(app);
+        await tester.pumpAndSettle();
+        final txtFound = find.byType(SearchBox);
+        expect(txtFound, findsOneWidget);
+        await tester.enterText(txtFound, 'sp');
+        await tester.pumpAndSettle();
+        final tiles = find.byType(ListTile);
+        expect(tiles, findsOneWidget);
+        // expect(tester.widget<ListTile>(tiles.first).key, equals(Key('ES')));
+        // // not the right language (we let english go through tho)
+        // await tester.enterText(txtFound, 'Espagne');
+        // await tester.pumpAndSettle();
+        // expect(tiles, findsNothing);
+        // await tester.pumpAndSettle();
+        // // dial codes
+        // await tester.enterText(txtFound, '33');
+        // await tester.pumpAndSettle();
+        // expect(tiles, findsWidgets);
+        // expect(tester.widget<ListTile>(tiles.first).key, equals(Key('FR')));
+      });
     });
 
-    // // // same test if added, fails
-    // testWidgets('should be properly sorted without favorites', (tester) async {
-    //   await tester.pumpWidget(builder());
-    //   await tester.pumpAndSettle();
-    //   final scaffold = find.byType(Scaffold);
-    //   expect(scaffold, findsOneWidget);
-    // });
+    group('With internationalization', () {
+      final app = MaterialApp(
+        locale: Locale('es', ''),
+        localizationsDelegates: [
+          ...GlobalMaterialLocalizations.delegates,
+          PhoneFieldLocalization.delegate,
+        ],
+        supportedLocales: [Locale('es', '')],
+        home: Scaffold(
+          body: CountrySelector(onCountrySelected: (c) {}),
+        ),
+      );
+
+      testWidgets('Should filter with text', (tester) async {
+        await tester.pumpWidget(app);
+        await tester.pump(Duration(seconds: 1));
+        await tester.pumpAndSettle();
+        final txtFound = find.byType(SearchBox);
+        expect(txtFound, findsOneWidget);
+        await tester.enterText(txtFound, 'esp');
+        await tester.pumpAndSettle();
+        final tiles = find.byType(ListTile);
+        expect(tiles, findsWidgets);
+        expect(tester.widget<ListTile>(tiles.first).key, equals(Key('ES')));
+        // not the right language (we let english go through tho)
+        await tester.enterText(txtFound, 'Espagne');
+        await tester.pumpAndSettle();
+        expect(tiles, findsNothing);
+        await tester.pumpAndSettle();
+        // dial codes (cant put it in another test widgets for some reason it fails)
+        await tester.enterText(txtFound, '33');
+        await tester.pumpAndSettle();
+        expect(tiles, findsWidgets);
+        expect(tester.widget<ListTile>(tiles.first).key, equals(Key('FR')));
+      });
+    });
   });
 }
