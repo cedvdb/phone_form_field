6ba1005886f43fe41a47768533d96900df12d435
Xavier H <xavier.hainaux@gmail.com>
2021-11-07 19:25:16 +0100

    Add swedish language (#78)
    * 4.3.0 (#73)
    
    * Minimize code duplication (#59)
    
    * Added common params to abstract class
    
    * Changed onCountrySelected to a ValueChanged object
    
    * Simplified code
    
    * phone_numbers_parser v4.0.1 (#60)
    
    * fix 54 (#63)
    
    * fix 54
    
    * changelog
    
    * update version
    
    * Rename invalidX to validX (#67)
    
    * rename validity
    
    * readme
    
    * Issue 69 params passthrough (#70)
    
    * passthrough
    
    * changelog
    
    * autovalidate mode
    
    * remove unnecessary print statement
    
    * Controller select all (#72)
    
    * passthrough
    
    * changelog
    
    * autovalidate mode
    
    * remove unnecessary print statement
    
    * ff
    
    * controllers refactor
    
    * controller
    
    * changelog
    
    * changelog
    
    Co-authored-by: Ajil Oommen <ajilo297@gmail.com>
    
    * new demo  (#74)
    
    * Minimize code duplication (#59)
    
    * Added common params to abstract class
    
    * Changed onCountrySelected to a ValueChanged object
    
    * Simplified code
    
    * phone_numbers_parser v4.0.1 (#60)
    
    * fix 54 (#63)
    
    * fix 54
    
    * changelog
    
    * update version
    
    * Rename invalidX to validX (#67)
    
    * rename validity
    
    * readme
    
    * Issue 69 params passthrough (#70)
    
    * passthrough
    
    * changelog
    
    * autovalidate mode
    
    * remove unnecessary print statement
    
    * Controller select all (#72)
    
    * passthrough
    
    * changelog
    
    * autovalidate mode
    
    * remove unnecessary print statement
    
    * ff
    
    * controllers refactor
    
    * controller
    
    * changelog
    
    * changelog
    
    * new doc
    
    Co-authored-by: Ajil Oommen <ajilo297@gmail.com>
    
    * fix focus issue (#76) (#77)
    
    * fix focus issue
    
    * fixes auto focus
    
    * Add SV language
    
    Co-authored-by: cedvdb <cedvandenbosch@gmail.com>
    Co-authored-by: Ajil Oommen <ajilo297@gmail.com>

---
+++ lib/l10n/generated/phone_field_localization_sv.dart
@@ -0,0 +1,750 @@
+import 'phone_field_localization.dart';
+
+/// The translations for Swedish (`sv`).
+class PhoneFieldLocalizationSv extends PhoneFieldLocalization {
+  PhoneFieldLocalizationSv([String locale = 'sv']) : super(locale);
+
+  @override
+  String get invalidPhoneNumber => 'Ogiltigt telefonnummer';
+
+  @override
+  String get invalidCountry => 'Ogiltigt land';
+
+  @override
+  String get invalidMobilePhoneNumber => 'Ogiltigt mobilnummer';
+
+  @override
+  String get invalidFixedLinePhoneNumber => 'Ogiltigt fast telefonnummer';
+
+  @override
+  String get requiredPhoneNumber => 'Obligatoriskt telefonnummer';
+
+  @override
+  String get noResultMessage => 'Inget resultat';
+
+  @override
+  String get ad_ => 'Andorra';
+
+  @override
+  String get ae_ => 'Förenade Arabemiraten';
+
+  @override
+  String get af_ => 'Afghanistan';
+
+  @override
+  String get ag_ => 'Antigua och Barbuda';
+
+  @override
+  String get ai_ => 'Anguilla';
+
+  @override
+  String get al_ => 'Albanien';
+
+  @override
+  String get am_ => 'Armenien';
+
+  @override
+  String get an_ => 'Nederländska Antillerna';
+
+  @override
+  String get ao_ => 'Angola';
+
+  @override
+  String get aq_ => 'Antarktis';
+
+  @override
+  String get ar_ => 'Argentina';
+
+  @override
+  String get as_ => 'Amerikanska Samoa';
+
+  @override
+  String get at_ => 'Österrike';
+
+  @override
+  String get au_ => 'Australien';
+
+  @override
+  String get aw_ => 'Aruba';
+
+  @override
+  String get ax_ => 'Åland';
+
+  @override
+  String get az_ => 'Azerbajdzjan';
+
+  @override
+  String get ba_ => 'Bosnien och Hercegovina';
+
+  @override
+  String get bb_ => 'Barbados';
+
+  @override
+  String get bd_ => 'Bangladesh';
+
+  @override
+  String get be_ => 'Belgien';
+
+  @override
+  String get bf_ => 'Burkina Faso';
+
+  @override
+  String get bg_ => 'Bulgarien';
+
+  @override
+  String get bh_ => 'Bahrain';
+
+  @override
+  String get bi_ => 'Burundi';
+
+  @override
+  String get bj_ => 'Benin';
+
+  @override
+  String get bl_ => 'S:t Barthélemy';
+
+  @override
+  String get bm_ => 'Bermuda';
+
+  @override
+  String get bn_ => 'Brunei';
+
+  @override
+  String get bo_ => 'Bolivia';
+
+  @override
+  String get br_ => 'Brasilien';
+
+  @override
+  String get bs_ => 'Bahamas';
+
+  @override
+  String get bt_ => 'Bhutan';
+
+  @override
+  String get bw_ => 'Botswana';
+
+  @override
+  String get by_ => 'Vitryssland';
+
+  @override
+  String get bz_ => 'Belize';
+
+  @override
+  String get ca_ => 'Kanada';
+
+  @override
+  String get cc_ => 'Kokosöarna';
+
+  @override
+  String get cd_ => 'Kongo-Kinshasa';
+
+  @override
+  String get cf_ => 'Centralafrikanska republiken';
+
+  @override
+  String get cg_ => 'Kongo-Brazzaville';
+
+  @override
+  String get ch_ => 'Schweiz';
+
+  @override
+  String get ci_ => 'Côte d’Ivoire';
+
+  @override
+  String get ck_ => 'Cooköarna';
+
+  @override
+  String get cl_ => 'Chile';
+
+  @override
+  String get cm_ => 'Kamerun';
+
+  @override
+  String get cn_ => 'Kina';
+
+  @override
+  String get co_ => 'Colombia';
+
+  @override
+  String get cr_ => 'Costa Rica';
+
+  @override
+  String get cu_ => 'Kuba';
+
+  @override
+  String get cv_ => 'Kap Verde';
+
+  @override
+  String get cx_ => 'Julön';
+
+  @override
+  String get cy_ => 'Cypern';
+
+  @override
+  String get cz_ => 'Tjeckien';
+
+  @override
+  String get de_ => 'Tyskland';
+
+  @override
+  String get dj_ => 'Djibouti';
+
+  @override
+  String get dk_ => 'Danmark';
+
+  @override
+  String get dm_ => 'Dominica';
+
+  @override
+  String get do_ => 'Dominikanska republiken';
+
+  @override
+  String get dz_ => 'Algeriet';
+
+  @override
+  String get ec_ => 'Ecuador';
+
+  @override
+  String get ee_ => 'Estland';
+
+  @override
+  String get eg_ => 'Egypten';
+
+  @override
+  String get er_ => 'Eritrea';
+
+  @override
+  String get es_ => 'Spanien';
+
+  @override
+  String get et_ => 'Etiopien';
+
+  @override
+  String get fi_ => 'Finland';
+
+  @override
+  String get fj_ => 'Fiji';
+
+  @override
+  String get fk_ => 'Falklandsöarna';
+
+  @override
+  String get fm_ => 'Mikronesien';
+
+  @override
+  String get fo_ => 'Färöarna';
+
+  @override
+  String get fr_ => 'Frankrike';
+
+  @override
+  String get ga_ => 'Gabon';
+
+  @override
+  String get gb_ => 'Storbritannien';
+
+  @override
+  String get gd_ => 'Grenada';
+
+  @override
+  String get ge_ => 'Georgien';
+
+  @override
+  String get gf_ => 'Franska Guyana';
+
+  @override
+  String get gg_ => 'Guernsey';
+
+  @override
+  String get gh_ => 'Ghana';
+
+  @override
+  String get gi_ => 'Gibraltar';
+
+  @override
+  String get gl_ => 'Grönland';
+
+  @override
+  String get gm_ => 'Gambia';
+
+  @override
+  String get gn_ => 'Guinea';
+
+  @override
+  String get gp_ => 'Guadeloupe';
+
+  @override
+  String get gq_ => 'Ekvatorialguinea';
+
+  @override
+  String get gr_ => 'Grekland';
+
+  @override
+  String get gs_ => 'Sydgeorgien och Sydsandwichöarna';
+
+  @override
+  String get gt_ => 'Guatemala';
+
+  @override
+  String get gu_ => 'Guam';
+
+  @override
+  String get gw_ => 'Guinea-Bissau';
+
+  @override
+  String get gy_ => 'Guyana';
+
+  @override
+  String get hk_ => 'Hongkong SAR';
+
+  @override
+  String get hn_ => 'Honduras';
+
+  @override
+  String get hr_ => 'Kroatien';
+
+  @override
+  String get ht_ => 'Haiti';
+
+  @override
+  String get hu_ => 'Ungern';
+
+  @override
+  String get id_ => 'Indonesien';
+
+  @override
+  String get ie_ => 'Irland';
+
+  @override
+  String get il_ => 'Israel';
+
+  @override
+  String get im_ => 'Isle of Man';
+
+  @override
+  String get in_ => 'Indien';
+
+  @override
+  String get io_ => 'Brittiska territoriet i Indiska oceanen';
+
+  @override
+  String get iq_ => 'Irak';
+
+  @override
+  String get ir_ => 'Iran';
+
+  @override
+  String get is_ => 'Island';
+
+  @override
+  String get it_ => 'Italien';
+
+  @override
+  String get je_ => 'Jersey';
+
+  @override
+  String get jm_ => 'Jamaica';
+
+  @override
+  String get jo_ => 'Jordanien';
+
+  @override
+  String get jp_ => 'Japan';
+
+  @override
+  String get ke_ => 'Kenya';
+
+  @override
+  String get kg_ => 'Kirgizistan';
+
+  @override
+  String get kh_ => 'Kambodja';
+
+  @override
+  String get ki_ => 'Kiribati';
+
+  @override
+  String get km_ => 'Komorerna';
+
+  @override
+  String get kn_ => 'S:t Kitts och Nevis';
+
+  @override
+  String get kp_ => 'Nordkorea';
+
+  @override
+  String get kr_ => 'Sydkorea';
+
+  @override
+  String get kw_ => 'Kuwait';
+
+  @override
+  String get ky_ => 'Caymanöarna';
+
+  @override
+  String get kz_ => 'Kazakstan';
+
+  @override
+  String get la_ => 'Laos';
+
+  @override
+  String get lb_ => 'Libanon';
+
+  @override
+  String get lc_ => 'S:t Lucia';
+
+  @override
+  String get li_ => 'Liechtenstein';
+
+  @override
+  String get lk_ => 'Sri Lanka';
+
+  @override
+  String get lr_ => 'Liberia';
+
+  @override
+  String get ls_ => 'Lesotho';
+
+  @override
+  String get lt_ => 'Litauen';
+
+  @override
+  String get lu_ => 'Luxemburg';
+
+  @override
+  String get lv_ => 'Lettland';
+
+  @override
+  String get ly_ => 'Libyen';
+
+  @override
+  String get ma_ => 'Marocko';
+
+  @override
+  String get mc_ => 'Monaco';
+
+  @override
+  String get md_ => 'Moldavien';
+
+  @override
+  String get me_ => 'Montenegro';
+
+  @override
+  String get mf_ => 'Saint-Martin';
+
+  @override
+  String get mg_ => 'Madagaskar';
+
+  @override
+  String get mh_ => 'Marshallöarna';
+
+  @override
+  String get mk_ => 'Nordmakedonien';
+
+  @override
+  String get ml_ => 'Mali';
+
+  @override
+  String get mm_ => 'Myanmar (Burma)';
+
+  @override
+  String get mn_ => 'Mongoliet';
+
+  @override
+  String get mo_ => 'Macao SAR';
+
+  @override
+  String get mp_ => 'Nordmarianerna';
+
+  @override
+  String get mq_ => 'Martinique';
+
+  @override
+  String get mr_ => 'Mauretanien';
+
+  @override
+  String get ms_ => 'Montserrat';
+
+  @override
+  String get mt_ => 'Malta';
+
+  @override
+  String get mu_ => 'Mauritius';
+
+  @override
+  String get mv_ => 'Maldiverna';
+
+  @override
+  String get mw_ => 'Malawi';
+
+  @override
+  String get mx_ => 'Mexiko';
+
+  @override
+  String get my_ => 'Malaysia';
+
+  @override
+  String get mz_ => 'Moçambique';
+
+  @override
+  String get na_ => 'Namibia';
+
+  @override
+  String get nc_ => 'Nya Kaledonien';
+
+  @override
+  String get ne_ => 'Niger';
+
+  @override
+  String get nf_ => 'Norfolkön';
+
+  @override
+  String get ng_ => 'Nigeria';
+
+  @override
+  String get ni_ => 'Nicaragua';
+
+  @override
+  String get nl_ => 'Nederländerna';
+
+  @override
+  String get no_ => 'Norge';
+
+  @override
+  String get np_ => 'Nepal';
+
+  @override
+  String get nr_ => 'Nauru';
+
+  @override
+  String get nu_ => 'Niue';
+
+  @override
+  String get nz_ => 'Nya Zeeland';
+
+  @override
+  String get om_ => 'Oman';
+
+  @override
+  String get pa_ => 'Panama';
+
+  @override
+  String get pe_ => 'Peru';
+
+  @override
+  String get pf_ => 'Franska Polynesien';
+
+  @override
+  String get pg_ => 'Papua Nya Guinea';
+
+  @override
+  String get ph_ => 'Filippinerna';
+
+  @override
+  String get pk_ => 'Pakistan';
+
+  @override
+  String get pl_ => 'Polen';
+
+  @override
+  String get pm_ => 'S:t Pierre och Miquelon';
+
+  @override
+  String get pn_ => 'Pitcairnöarna';
+
+  @override
+  String get pr_ => 'Puerto Rico';
+
+  @override
+  String get ps_ => 'Palestinska territorierna';
+
+  @override
+  String get pt_ => 'Portugal';
+
+  @override
+  String get pw_ => 'Palau';
+
+  @override
+  String get py_ => 'Paraguay';
+
+  @override
+  String get qa_ => 'Qatar';
+
+  @override
+  String get re_ => 'Réunion';
+
+  @override
+  String get ro_ => 'Rumänien';
+
+  @override
+  String get rs_ => 'Serbien';
+
+  @override
+  String get ru_ => 'Ryssland';
+
+  @override
+  String get rw_ => 'Rwanda';
+
+  @override
+  String get sa_ => 'Saudiarabien';
+
+  @override
+  String get sb_ => 'Salomonöarna';
+
+  @override
+  String get sc_ => 'Seychellerna';
+
+  @override
+  String get sd_ => 'Sudan';
+
+  @override
+  String get se_ => 'Sverige';
+
+  @override
+  String get sg_ => 'Singapore';
+
+  @override
+  String get sh_ => 'S:t Helena';
+
+  @override
+  String get si_ => 'Slovenien';
+
+  @override
+  String get sj_ => 'Svalbard och Jan Mayen';
+
+  @override
+  String get sk_ => 'Slovakien';
+
+  @override
+  String get sl_ => 'Sierra Leone';
+
+  @override
+  String get sm_ => 'San Marino';
+
+  @override
+  String get sn_ => 'Senegal';
+
+  @override
+  String get so_ => 'Somalia';
+
+  @override
+  String get sr_ => 'Surinam';
+
+  @override
+  String get ss_ => 'Sydsudan';
+
+  @override
+  String get st_ => 'São Tomé och Príncipe';
+
+  @override
+  String get sv_ => 'El Salvador';
+
+  @override
+  String get sy_ => 'Syrien';
+
+  @override
+  String get sz_ => 'Swaziland';
+
+  @override
+  String get tc_ => 'Turks- och Caicosöarna';
+
+  @override
+  String get td_ => 'Tchad';
+
+  @override
+  String get tg_ => 'Togo';
+
+  @override
+  String get th_ => 'Thailand';
+
+  @override
+  String get tj_ => 'Tadzjikistan';
+
+  @override
+  String get tk_ => 'Tokelauöarna';
+
+  @override
+  String get tl_ => 'Östtimor';
+
+  @override
+  String get tm_ => 'Turkmenistan';
+
+  @override
+  String get tn_ => 'Tunisien';
+
+  @override
+  String get to_ => 'Tonga';
+
+  @override
+  String get tr_ => 'Turkiet';
+
+  @override
+  String get tt_ => 'Trinidad och Tobago';
+
+  @override
+  String get tv_ => 'Tuvalu';
+
+  @override
+  String get tw_ => 'Taiwan';
+
+  @override
+  String get tz_ => 'Tanzania';
+
+  @override
+  String get ua_ => 'Ukraina';
+
+  @override
+  String get ug_ => 'Uganda';
+
+  @override
+  String get us_ => 'USA';
+
+  @override
+  String get uy_ => 'Uruguay';
+
+  @override
+  String get uz_ => 'Uzbekistan';
+
+  @override
+  String get va_ => 'Vatikanstaten';
+
+  @override
+  String get vc_ => 'S:t Vincent och Grenadinerna';
+
+  @override
+  String get ve_ => 'Venezuela';
+
+  @override
+  String get vg_ => 'Brittiska Jungfruöarna';
+
+  @override
+  String get vi_ => 'Amerikanska Jungfruöarna';
+
+  @override
+  String get vn_ => 'Vietnam';
+
+  @override
+  String get vu_ => 'Vanuatu';
+
+  @override
+  String get wf_ => 'Wallis- och Futunaöarna';
+
+  @override
+  String get ws_ => 'Samoa';
+
+  @override
+  String get ye_ => 'Jemen';
+
+  @override
+  String get yt_ => 'Mayotte';
+
+  @override
+  String get za_ => 'Sydafrika';
+
+  @override
+  String get zm_ => 'Zambia';
+
+  @override
+  String get zw_ => 'Zimbabwe';
+}
