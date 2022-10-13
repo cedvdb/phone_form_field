e890f519748947912fd143ba0cc342b03ff0771e
Mohanned Binmiskeen <mohn93@gmail.com>
2022-10-13 14:51:11 +0200

    Fix keyboard jitter

--- lib/src/widgets/country_selector/country_selector_navigator.dart
+++ lib/src/widgets/country_selector/country_selector_navigator.dart
@@ -433,6 +433,7 @@ class DraggableModalBottomSheetNavigator extends CountrySelectorNavigator {
           );
         },
       ),
+      useRootNavigator: true,
       isScrollControlled: true,
     );
   }
