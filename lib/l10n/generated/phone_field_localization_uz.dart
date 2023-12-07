0005f21340945ac5bb159ac1ac86e59af781fd10
cedvdb <cedvandenbosch@gmail.com>
2023-12-07 16:24:11 +0100

    v.8.1.1 (#204)
    * Fix the arabic translation and add the persian translation (#180)
    
    * Fix the arabic translation
    
    * Add the persian translation
    
    * change Turkey to Türkiye (#186)
    
    Co-authored-by: ferdogan <fatih.erdogan@innology.com.tr>
    
    * fix libya arabic and english name (#189)
    
    * The Uzbek language package has been added (#192)
    
    * Create uz.arb
    
    * Update uz.arb
    
    * change lang
    
    * bug fix
    
    * Update README.md (#193)
    
    * FEAT: show selected country iso code feature implemented (#190)
    
    Co-authored-by: Hemant <hemant.paneru@ebpearls.com>
    
    * set autofocus to false in example
    
    * feat: add parameter onTapOutside (#195)
    
    * feat: add Norwegian language (#203)
    
    * feat: add Norwegian language
    
    * docs: add `nb` language to README
    
    * v8.1.1
    
    ---------
    
    Co-authored-by: farzadaqajani <97414059+farzadaqajani@users.noreply.github.com>
    Co-authored-by: Fatih <56973746+themmfa@users.noreply.github.com>
    Co-authored-by: ferdogan <fatih.erdogan@innology.com.tr>
    Co-authored-by: Mohaned Benmesken <mohn93@gmail.com>
    Co-authored-by: Akbarali <39323182+akbarali1@users.noreply.github.com>
    Co-authored-by: Hemant Paneru HP <39861156+Hpaneru@users.noreply.github.com>
    Co-authored-by: Hemant <hemant.paneru@ebpearls.com>
    Co-authored-by: Andres Garcia <afgarcia0479@misena.edu.co>
    Co-authored-by: Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>

--- lib/l10n/generated/phone_field_localization_uz.dart
+++ lib/l10n/generated/phone_field_localization_uz.dart
@@ -14,7 +14,8 @@ class PhoneFieldLocalizationUz extends PhoneFieldLocalization {
   String get invalidMobilePhoneNumber => 'Telfon raqami noto‘g‘ri';
 
   @override
-  String get invalidFixedLinePhoneNumber => 'Ruxsat etilgan telefon raqami yaroqsiz';
+  String get invalidFixedLinePhoneNumber =>
+      'Ruxsat etilgan telefon raqami yaroqsiz';
 
   @override
   String get requiredPhoneNumber => 'Telfon raqami majburiy';
