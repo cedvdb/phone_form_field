820be26c9ca9884ebc16f53d6edb32fa02adfdc0
Emmanuel Ferdman <emmanuelferdman@gmail.com>
2024-06-27 03:12:12 +0300

    feat: add support to Hebrew

---
+++ lib/src/localization/generated/phone_field_localization_impl_he.dart
@@ -0,0 +1,34 @@
+import 'phone_field_localization_impl.dart';
+
+/// The translations for Hebrew (`he`).
+class PhoneFieldLocalizationImplHe extends PhoneFieldLocalizationImpl {
+  PhoneFieldLocalizationImplHe([super.locale = 'he']);
+
+  @override
+  String get invalidPhoneNumber => 'מספר טלפון לא חוקי';
+
+  @override
+  String get invalidCountry => 'מדינה לא חוקית';
+
+  @override
+  String get invalidMobilePhoneNumber => 'מספר טלפון נייד לא חוקי';
+
+  @override
+  String get invalidFixedLinePhoneNumber => 'מספר טלפון קווי לא חוקי';
+
+  @override
+  String get requiredPhoneNumber => 'מספר טלפון נדרש';
+
+  @override
+  String selectACountrySemanticLabel(String countryName, String dialCode) {
+    return 'בחר מדינה. הבחירה הנוכחית: $countryName $dialCode';
+  }
+
+  @override
+  String get phoneNumber => 'מספר טלפון';
+
+  @override
+  String currentValueSemanticLabel(String currentValue) {
+    return 'הערך הנוכחי: $currentValue';
+  }
+}
